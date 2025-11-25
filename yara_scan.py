63a13c338a309454cfabb75f42bcba96aa71fcc1
Stark3rn <lbastardnaminzo@gmail.com>
2025-11-25 11:55:26 +0100

    added yara rules & recursive checking of 'rules' directory

--- yara_scan.py
+++ yara_scan.py
@@ -3,18 +3,18 @@ import yara
 
 def scan_file_with_yara(file_path: str, rules_dir: str = "./rules") -> dict:
     matches_dict = {}
-
-    for rule_file in os.listdir(rules_dir):
-        if rule_file.endswith((".yar", ".yara")):
-            rule_path = os.path.join(rules_dir, rule_file)
-            try:
-                rules = yara.compile(filepath=rule_path)
-                matches = rules.match(file_path)
-                if matches:
-                    matches_dict[rule_file] = [m.rule for m in matches]
-            except yara.SyntaxError as e:
-                print(f"Erreur syntaxe dans la règle {rule_file}: {e}")
-            except Exception as e:
-                print(f"Erreur lors du scan avec {rule_file}: {e}")
+    for root, _, files in os.walk(rules_dir):
+        for rule_file in files:
+            if rule_file.endswith((".yar", ".yara")):
+                rule_path = os.path.join(root, rule_file)
+                try:
+                    rules = yara.compile(filepath=rule_path)
+                    matches = rules.match(file_path)
+                    if matches:
+                        matches_dict[rule_path] = [m.rule for m in matches]
+                except yara.SyntaxError as e:
+                    print(f"Syntax error in rule {rule_path}: {e}")
+                except Exception as e:
+                    print(f"Error scanning with {rule_path}: {e}")
 
     return matches_dict
